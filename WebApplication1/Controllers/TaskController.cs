f86bce55cf2a8f2fdf684feaa0885998ef5c9126
Ben Smith <benSmith1981@users.noreply.github.com>
2023-11-02 16:48:41 +0000

    Add project files.

---
+++ WebApplication1/Controllers/TaskController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+public class TaskController : Controller
+{
+    public IActionResult Index()
+    {
+        // Simulate fetching tasks from a database or other data source
+        var tasks = new List<TaskModel>
+        {
+            new TaskModel { Name = "Task 1", Description = "Description 1", DueDate = DateTime.Now.AddDays(3), Status = "Incomplete" },
+            new TaskModel { Name = "Task 2", Description = "Description 2", DueDate = DateTime.Now.AddDays(1), Status = "Incomplete" },
+            new TaskModel { Name = "Task 3", Description = "Description 3", DueDate = DateTime.Now.AddDays(7), Status = "Completed" }
+        };
+
+        return View(tasks);
+    }
+}
---
+++ WebApplication1/Models/TaskModel.cs
@@ -0,0 +1,8 @@
+﻿using System;
+public class TaskModel
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime DueDate { get; set; }
+    public string Status { get; set; }
+}
